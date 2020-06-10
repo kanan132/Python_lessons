caa32d7923ed48d1a1e9ccaf14c66016f208d32d
sam01ln12 <63307379+sam01ln12@users.noreply.github.com>
2020-06-10 16:11:30 +0400

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,8 +1,9 @@
 for i in range(1, 101):
-    if i%15==0:
-        print('{}: FizzBuzz'.format(i))
-    elif i%3==0:
-        print('{}: Fizz'.format(i))
+    if i%3==0:
+        if i%5==0:
+            print('{}: FizzBuzz'.format(i))
+        else:
+            print('{}: Fizz'.format(i))
     elif i%5==0:
         print('{}: Buzz'.format(i))
     else:
