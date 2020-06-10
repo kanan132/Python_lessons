4548c5db2a1363ad8fbb740f4c1ca49ca9d9c3c5
sam01ln12 <63307379+sam01ln12@users.noreply.github.com>
2020-06-10 16:54:21 +0400

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,10 +1,13 @@
+fizz_buzz_list=[]
 for i in range(1, 101):
     if i%3==0:
         if i%5==0:
-            print('{}: FizzBuzz'.format(i))
+            fizz_buzz_list.append('FizzBuzz')
         else:
-            print('{}: Fizz'.format(i))
+            fizz_buzz_list.append('Fizz')
     elif i%5==0:
-        print('{}: Buzz'.format(i))
+        fizz_buzz_list.append('Buzz')
     else:
-        print('{}: Nothing'.format(i))
+        fizz_buzz_list.append('Nothing')
+
+print('Fizz: {}, Buzz: {}, FizzBuzz: {}, Nothing: {}'.format(fizz_buzz_list.count('Fizz'), fizz_buzz_list.count('Buzz'),fizz_buzz_list.count('FizzBuzz'),fizz_buzz_list.count('Nothing')))
