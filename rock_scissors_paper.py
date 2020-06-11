d7b0368a03ab2850fb7294cac37618cf8a0e0415
sam01ln12 <63307379+sam01ln12@users.noreply.github.com>
2020-06-11 17:17:51 +0400

    Update rock_scissors_paper.py

--- rock_scissors_paper.py
+++ rock_scissors_paper.py
@@ -5,32 +5,43 @@ while True:
     selection = input('Make your choice: Rock or Scissors or Paper or press q to finish the game: ')
     comp_choice = random.choice(l)
     if selection !='q':
-        selection.capitalize()
+        s = selection.capitalize()
         game+=1
+        print(f'Human Choice: {s}\nComputer Choice: {comp_choice}')
         
         if comp_choice=='Rock':
-            if selection == 'Paper':
+            if s == 'Paper':
                 human+=1
-            elif selection == 'Scissors':
+                print('Human Wins!')
+            elif s == 'Scissors':
                 comp+=1
+                print('Computer Wins!')
             else:
                 draw+=1
+                print('Draw!')
         elif comp_choice=='Scissors':
-            if selection == 'Rock':
+            if s == 'Rock':
                 human+=1
-            elif selection == 'Paper':
+                print('Human Wins!')
+            elif s == 'Paper':
                 comp+=1
+                print('Computer Wins!')
             else:
                 draw+=1
+                print('Draw!')
         elif comp_choice == 'Paper':
-            if selection == 'Paper':
+            if s == 'Paper':
                 draw+=1
-            elif selection == 'Scissors':
+                print('Draw!')
+            elif s == 'Scissors':
                 human+=1
+                print('Human Wins!')
             else:
                 comp+=1
+                print('Computer Wins!')
 
         print(f'Game: {game}, Computer: {comp}, Human: {human} Draw: {draw}')
+        print('-'*40)
     else:
         print('Ending the game')
         break
