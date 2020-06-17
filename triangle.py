97003bbef3b8d6d007fc8e7feee584f91fb4c8ca
Samad Mirzayev <samed.mirzayev@outlook.com>
2020-06-17 13:42:56 +0400

    Change logic of script and wrote using function

--- triangle.py
+++ triangle.py
@@ -1,10 +1,13 @@
-length = int(input('Enter number '))
-symbol = input('Enter the symbol ')
-width = 2 * length
-for i in range(length):
-   for n in range(width):
-      print(end=' ')
-   width -=1
-   for n in range(i+1):
-      print(symbol, end=' ')
-   print(' ')
+length = int(input('Enter number: '))
+symbol = input('Enter the symbol: ')
+
+def print_triangle(length, symbol):
+   for i in range(length):
+      print(end=' '*(length-i))
+      for j in range(i+1):
+         print(symbol, end=' ')
+      print(' ')
+   
+   
+   
+print_triangle(length, symbol)
