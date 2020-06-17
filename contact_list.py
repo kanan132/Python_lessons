d200645fbb1c7f0605fc8da151d746129617c029
Samad Mirzayev <samed.mirzayev@outlook.com>
2020-06-17 15:17:30 +0400

    Changed logic and wrote script using function

--- contact_list.py
+++ contact_list.py
@@ -1,19 +1,68 @@
-contact_list = {'Ayaz':5686667, 'Arif':3464646464, 'Bekhtiyar':43448484, 'Javanshir': 4368683653, 'Elman': 43656565656, 'Elshan':3636436346, 'Farhad':43436536563}
+contact_list = {'Ayaz':{
+        'name':'Ayaz',
+        'phone':5686667,
+        'occupation': 'IT Specialist'}, 
+    'Arif':{
+        'name':'Arif',
+        'phone':3464646464,
+        'occupation':'Seller'},
+    'Bakhtiyar':{
+        'name':'Bakhtiyar',
+        'phone':43448484,
+        'occupation':'Driver'},
+    'Javanshir': {
+        'name':'Javanshir',
+        'phone':4368683653,
+        'occupation':'Engineer'},
+    'Elman': {
+        'name':'Elman',
+        'phone':43656565656,
+        'occupation':'Administrator'},
+    'Elshan':{
+        'name':'Elshan',
+        'phone':3636436346,
+        'occupation':'Sportsman'},
+    'Farhad':{
+        'name':'Farhad',
+        'phone':43436536563,
+        'occupation':'Scientist'}
+    }
+def add_person(nick, name, phone, occupation):
+    contact_list[nick] = {
+        'name':name,
+        'phone':phone,
+        'occupation':occupation
+    }
+def print_person_info(nick):
+    return 'Name: {}\nPhone: {}\nOccupation: {}'.format(contact_list[nick]['name'],contact_list[nick]['phone'], contact_list[nick]['occupation'])
+
 while True:
-    name = input('Type whose phone number you want to see: ')
-    n = name.capitalize()
-    if n in contact_list.keys():
-        print(f'{n}\'s phone number is {contact_list[n]}')
-    else:
-        question = input('Do you want to add this person\'s number? ')
-        answer = question.lower()
-        if answer == 'yes':
+    nick = input('Type about whom you want to see information: ')
+    a = nick.capitalize()
+    if contact_list.get(a, 'DEFAULT') == 'DEFAULT':
+        print('There is no information about this person.\n')
+        question = input(f'Do you want to add information about {a} ? [yes/no] ')
+        if question == 'yes':
+            name = input('Please enter the name: ')
             phone = int(input('Please enter the phone number: '))
-            contact_list[n] = phone
-            print(f'{n}\'s phone number is {contact_list[n]}')
+            occupation = input('Please enter the occupation: ')
+            add_person(a, name.capitalize(), phone, occupation.capitalize())
+            print(print_person_info(a))
         else:
             print('Exiting program')
             break
+    else:
+        print(print_person_info(a))
+            
 
-for k, v in contact_list.items():
-    print(k, v)
+l_question = input('Do you want to see entire contact list? [yes/no] ')
+if l_question == 'yes':
+    for v in contact_list.values():
+        for key, value in v.items():
+            print(f'{key} : {value}')
+        print('-'*40)
+else:
+    print('Good Bye')
+
+
+
