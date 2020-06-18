dfbbea62a2bed586204cb928eddd41e33659ec84
Samad Mirzayev <samed.mirzayev@outlook.com>
2020-06-18 16:13:20 +0400

    Add file tolstoy.txt from which I read information Using tolstoy_biography_file.py I count number of symbols and vowels

---
+++ work_with_files/tolstoy_biography_file.py
@@ -0,0 +1,17 @@
+big_string = ''
+with open('tolstoy.txt', 'r') as inf:
+    for line in inf:
+        line = line.strip()
+        big_string+=line
+vowels = ['e', 'u', 'i', 'o', 'a', ]
+def count_vowels(string):
+    counter = 0
+    for letter in big_string:
+        a = letter.lower()
+        if a in vowels:
+            counter +=1
+    return counter
+
+print(f'Number of symbols is {len(big_string)} and number of vowels is {count_vowels(big_string)}')
+
+
